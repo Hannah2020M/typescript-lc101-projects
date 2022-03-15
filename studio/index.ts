5985ec119aacc5e5fc6e2d9a816a6100a9c2a9ec
Hannah2020M <coder2020aug@gmail.com>
2022-03-15 10:09:18 -0500

    basic index file with only mySum

--- studio/index.ts
+++ studio/index.ts
@@ -18,42 +18,42 @@ let myRocket = new Rocket('my rocket', 0);
 const mySum = myRocket.sumMass(myCargoArray);
 console.log('my sum is: 'mySum');
 
-let falcon9: Rocket = new Rocket('Falcon 9', 7500);
+// let falcon9: Rocket = new Rocket('Falcon 9', 7500);
 
-let astronauts: Astronaut[] = [
-    new Astronaut(75, 'Mae'),
-    new Astronaut(81, 'Sally'),
-    new Astronaut(99, 'Charles')
-];
+// let astronauts: Astronaut[] = [
+//     new Astronaut(75, 'Mae'),
+//     new Astronaut(81, 'Sally'),
+//     new Astronaut(99, 'Charles')
+// ];
 
-for (let i = 0; i < astronauts.length; i++) {
-    let astronaut = astronauts[i];
-    let status = '';
-    if (falcon9.addAstronaut(astronaut)) {
-        status = "On board";
-    } else {
-        status = "Not on board";
-    }
-    console.log(`${astronaut.name}: ${status}`);
-}
+// for (let i = 0; i < astronauts.length; i++) {
+//     let astronaut = astronauts[i];
+//     let status = '';
+//     if (falcon9.addAstronaut(astronaut)) {
+//         status = "On board";
+//     } else {
+//         status = "Not on board";
+//     }
+//     console.log(`${astronaut.name}: ${status}`);
+// }
 
-let cargo: Cargo[] = [
-    new Cargo(3107.39, "Satellite"),
-    new Cargo(1000.39, "Space Probe"),
-    new Cargo(753, "Water"),
-    new Cargo(541, "Food"),
-    new Cargo(2107.39, "Tesla Roadster"),
-];
+// let cargo: Cargo[] = [
+//     new Cargo(3107.39, "Satellite"),
+//     new Cargo(1000.39, "Space Probe"),
+//     new Cargo(753, "Water"),
+//     new Cargo(541, "Food"),
+//     new Cargo(2107.39, "Tesla Roadster"),
+// ];
 
-for (let i = 0; i < cargo.length; i++) {
-    let c = cargo[i];
-    let loaded = '';
-    if (falcon9.addCargo(c)) {
-        loaded = "Loaded"
-    } else {
-        loaded = "Not loaded"
-    }
-    console.log(`${c.material}: ${loaded}`);
-}
+// for (let i = 0; i < cargo.length; i++) {
+//     let c = cargo[i];
+//     let loaded = '';
+//     if (falcon9.addCargo(c)) {
+//         loaded = "Loaded"
+//     } else {
+//         loaded = "Not loaded"
+//     }
+//     console.log(`${c.material}: ${loaded}`);
+// }
 
-console.log(`Final cargo and astronaut mass: ${falcon9.currentMassKg()} kg.`);
+// console.log(`Final cargo and astronaut mass: ${falcon9.currentMassKg()} kg.`);
